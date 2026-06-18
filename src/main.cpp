faed92a3ee579d603a76d3783c70b22b2ab06360
AdityaMaulanaFirdaus <2503010003@unper.ac.id>
2026-06-18 11:57:16 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -70,9 +70,68 @@ void riwayatBuku() {
 
 // --- MENU UTAMA ---
 int main() {
-    // Anggota 3: Panggil fungsi loadDatabaseBuku() di sini nanti
+    loadDatabaseBuku(); 
+    int pilihan;
     
-    // Anggota 2: Buat looping do-while & switch-case menu (1-9) di sini
+    do {
+        cout << "\n========================================\n";
+        cout << "     SISTEM MANAJEMEN PERPUSTAKAAN      \n";
+        cout << "========================================\n";
+        cout << " [1] Tampilkan Semua Buku\n";
+        cout << " [2] Cari Buku\n";
+        cout << " [3] Tambah Buku Baru\n";
+        cout << " [4] Hapus Buku\n";
+        cout << " [5] Pinjam Buku\n";
+        cout << " [6] Kembalikan Buku\n";
+        cout << " [7] Urutkan Buku (A-Z)\n";
+        cout << " [8] Rekomendasi & Riwayat\n";
+        cout << " [9] Keluar Sistem\n";
+        cout << "========================================\n";
+        cout << " Pilih menu (1-9): "; cin >> pilihan;
+        
+        switch (pilihan) {
+            case 1:
+                tampilBuku();
+                break;
+                
+            case 2:
+                cariBuku();
+                break;
+                
+            case 3:
+                tambahBuku();
+                break;
+                
+            case 4:
+                hapusBuku();
+                break;
+                
+            case 5:
+                pinjamBuku();
+                break;
+                
+            case 6:
+                kembalikanBuku();
+                break;
+                
+            case 7:
+                sortingBuku();
+                break;
+                
+            case 8:
+                rekomendasiBuku();
+                riwayatBuku();
+                break;
+                
+            case 9:
+                cout << "\n>> Keluar dari sistem. Terima kasih!\n";
+                break;
+                
+            default:
+                cout << "\n[Peringatan] Pilihan tidak valid! Masukkan angka 1-9.\n";
+                break;
+        }
+    } while (pilihan != 9);
     
     return 0;
 }
